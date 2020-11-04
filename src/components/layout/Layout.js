fad69558431176a355d66c090abb3a7f69c02ceb
Liam Hawks <liamhawks@gmail.com>
2020-11-04 17:52:06 +0000

    fix: move reset styling

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./Header"
-import "./reset.scss"
+import "../../styles/reset.scss"
 import "./layout.scss"
 
 export const PureLayout = ({ children, data }) => {
